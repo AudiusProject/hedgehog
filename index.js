e04d3863961f69018b3407ce0d5468215c79af5f
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2019-04-15 16:55:28 -0700

    semantics

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const WalletManager = require('./walletManager')
 const Authentication = require('./authentication')
 const Hedgehog = require('./hedgehog')
 
-module.exports.Hedgehog = Hedgehog
-module.exports.WalletManager = WalletManager
-module.exports.Authentication = Authentication
+module.exports = {
+  Hedgehog,
+  WalletManager,
+  Authentication,
+}
